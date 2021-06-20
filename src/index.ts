6d0ee3814c961ed74e8894fdbbf324da1121d560
aaronchen <aaron.chen@onedegree.hk>
2021-06-20 18:17:08 +0800

    add module-alias to run the files in dist

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// import 'module-alias/register';  // import this package if want to run the files in dist
+
 import { example1, example2, example3 } from './examples/for-async-await';
 
 function main() {
