b77c41d0e445a930135b323a0269001b07bb0ff9
pjchender <pjchender@gmail.com>
2021-08-01 22:54:33 +0800

    fix: update npm packages and use eslint-plugin-pjchender

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,11 @@
 const allExtensions = ['.ts', '.tsx', '.d.ts', '.js', '.jsx', '.json'];
 
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
+  extends: ['pjchender/typescript'],
   parserOptions: {
     ecmaVersion: 12,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    tsconfigRootDir: __dirname, // 如果是 monorepo 建議要加這行，避免找到外層的 tsconfig
 
     /**
      * 因為有使用 recommended-requiring-type-checking
@@ -16,55 +13,12 @@ module.exports = {
      * 讓它能夠取得正確的 type information
      * */
     project: 'tsconfig.eslint.json',
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    tsconfigRootDir: __dirname, // 如果是 monorepo 建議要加這行，避免找到外層的 tsconfig
   },
-  plugins: ['@typescript-eslint', 'simple-import-sort'],
-  extends: [
-    'eslint:recommended',
-    'plugin:jest/recommended',
-    'plugin:import/errors',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-  ],
+
   rules: {
     'no-console': 'off',
-    '@typescript-eslint/no-unused-vars': 'off', // 這個規則原本 ts 就內建了
-    'no-use-before-define': [
-      'error',
-      { functions: false, classes: true, variables: true },
-    ],
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      {
-        functions: false,
-        classes: true,
-        variables: true,
-        typedefs: false,
-        ignoreTypeReferences: true,
-      },
-    ],
     'import/extensions': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: [
-          '**/*.test.ts',
-          '**/*.test.tsx',
-          '**/*.stories.ts',
-          '**/*.stories.tsx',
-        ],
-      },
-    ],
-    'import/prefer-default-export': 'off',
-    'import/first': 'error',
-    'import/newline-after-import': 'error',
-    'import/no-duplicates': 'error',
     'import/order': 'off',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
   },
   settings: {
     node: {
