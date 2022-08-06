9ec4f323315920e7b4672ac40cdf2ccc003fba49
pjchender <pjchender@gmail.com>
2022-08-06 15:29:01 +0800

    feat: using dynamic import to autoload module-alias plugin

--- README.md
+++ README.md
@@ -4,24 +4,26 @@
 
 ## Usage
 
-Run in development:
+### Run in development
 
 ```bash
 # run in development
 yarn dev
 ```
 
-Run in production:
+### Run in production
 
-- remember to comment out `import 'module-alias/register'` in the `index.ts` which is used for import alias
+If you use import alias, for example, `@/utils/foobar`, then it will need the `'module-alias/register'`.
 
 > cause `module-alias/register` is not compatible with ts-node, so you should import this module only when you want to build the package with import-alias.
+
+We use dynamic import here for the `'module-alias/register'`, so remember set the `NODE_ENV` to the `production` which will import the package.
 
 ```bash
 # build and run the dist files
 # remember to comment out `import 'module-alias/register'` in the `index.ts` which is used for import alias
 yarn build
-yarn start
+NODE_ENV=production yarn start
 ```
 
 ## Reference
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
-// import 'module-alias/register'; // import this package if want to run the files in dist
+/* eslint-disable @typescript-eslint/no-unsafe-member-access,  @typescript-eslint/no-floating-promises */
 
-import { example1, example2, example3 } from './examples/for-async-await';
+async function main() {
+  /**
+   * Bootstrap
+   * */
+  if (process.env.NODE_ENV === 'production') {
+    // import the package if want to use the import alias, i.e., @/utils/foobar, in the production
+    // @ts-expect-error: no types definition
+    await import('module-alias/register');
+  }
 
-function main() {
-  example1();
-  example2();
-  example3();
+  /**
+   * Import modules using import alias with dynamic import...
+   */
+  const { sleep } = await import('@/utils/sleep');
+  const { example1 } = await import('@/examples/for-async-await');
+
+  /**
+   * Write the code below...
+   */
+  await sleep('sleep', 1000);
+  await example1();
 }
 
-main();
+main().catch((error) => {
+  console.error('[main] error', error);
+});
